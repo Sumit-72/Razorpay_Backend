aeb2d987e78b9aed910738e5ef2cb2a29fe5be27
Sumit Shekhar <shekharsumit65@gmail.com>
2025-05-19 16:26:32 +0530

    Fixed vercel.json

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 
 const connectToMongo = async () => {
     try {
-        await connect('mongodb+srv://Psquare:mongoDBatlas@mernapp.xh4pt.mongodb.net/?retryWrites=true&w=majority&appName=MERNapp', {
+        await connect('//', {
             dbName: "paymentGateway",
         });
         console.log("---***Database Connected Successfully***---")
